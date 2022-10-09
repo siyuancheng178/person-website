567580672c58f60eda79a4d01c3df1dd6fe8c26f
Siyuan Cheng <siyuanc6794@gmail.com>
2022-10-09 17:13:56 +0800

    update data

--- src/data/about/degrees.js
+++ src/data/about/degrees.js
@@ -3,7 +3,7 @@ const degrees = [
     school: 'National University of Singapore',
     degree: 'BEng, Computer Engineering (Honours)',
     link: 'https://www.nus.edu.sg/',
-    year: '2020.08 - 2023.06',
+    year: '2020.08 - Present',
   },
 ];
 
